b32daf6c606ee8ed6b5ef2d793c33cc23c9e27d9
Arthur-Boddaert <124796061+Arthur-Boddaert@users.noreply.github.com>
2024-01-31 12:13:03 +0100

    lecture fichier compresser

--- succinct_multiple_alignment.py
+++ succinct_multiple_alignment.py
@@ -7,11 +7,12 @@ import os
 from succinct_column import SuccinctColumn
 from Bio import SeqIO
 import pysdsl
+import gzip
 
 # Class definition
 class SuccinctMultipleAlignment:
 
-    def __init__(self, fasta_file, nb_columns=1000, vector="SDVector"):
+    def __init__(self, fasta_file, nb_columns=1000, vector="SDVector", compressed=False):
         """
         Build the succinct multiple alignment as a list of objects SuccinctColumn.
 
@@ -27,9 +28,14 @@ class SuccinctMultipleAlignment:
         None
         """
         self.__multialign = []
-        self.__size, self.__length = self.fetch_alignment_size(fasta_file)
-        for position in range(0, self.__length, nb_columns):
-            self.__multialign += self.fetch_column_V2(fasta_file, position, nb_columns, vector)
+        if compressed:
+            self.__size, self.__length = self.fetch_alignment_size_compress(fasta_file)
+            for position in range(0, self.__length, nb_columns):
+                self.__multialign += self.fetch_column_compress(fasta_file, position, nb_columns, vector)
+        else:
+            self.__size, self.__length = self.fetch_alignment_size(fasta_file)
+            for position in range(0, self.__length, nb_columns):
+                self.__multialign += self.fetch_column(fasta_file, position, nb_columns, vector)
 
     def __len__(self):
         return len(self.__multialign)        
@@ -68,40 +74,39 @@ class SuccinctMultipleAlignment:
         return (seq_count, align_length)
 
     @staticmethod
-    def fetch_column(fasta_file, position):
+    def fetch_alignment_size_compress(fasta_file):
         """
-        Read the FASTA file and store the nucleotide at a specified position in each sequence.
+        Read the FASTA file and store the size and the number of sequences.
 
         Parameters:
         -----------
         fasta_file : str
             A FASTA file containing multiple sequences aligned.
-        position : int
-            The position of the nucleotide to store in each sequence.
-        
+
         Return:
         -------
-        str :
-            The nucleotides of every sequences at a specified position.
+        (int, int):
+            - The number of sequences.
+            - The length of the sequences (which is supposed to be the same for every sequence).
         """
-        with open (fasta_file, "r") as fileIn:
-            column_seq = ""
-            for line in fileIn.readlines():
-                if line.startswith(">"):  # New sequence, reset variables
-                    len_still_to_read = position
-                elif len_still_to_read != -1:  # If the nucleotide of the sequence have already been stored.
-                    len_seq = len(line.strip())
-                    if len_still_to_read >= len_seq:  # Instead of rebuilding the sequence, we only store the number of nt still to read.
-                        len_still_to_read -= len_seq
-                    else:
-                        nt= line[len_still_to_read].upper()
-                        if nt not in {'A','T','C','G','-'}:
-                            raise ValueError("There is an invalid nucleotide in the sequence.")
-                        column_seq += nt
-                        len_still_to_read = -1
-        return column_seq
+        if not os.path.isfile(fasta_file):
+            raise FileNotFoundError(fasta_file)
+
+        with gzip.open(fasta_file, 'rt') as handle:
+            seq_count = 0  # sequence counter
+            align_length = None
+            for record in SeqIO.parse(handle, 'fasta'):
+                if align_length is None:
+                    print (len(record.seq))
+                    align_length = len(record.seq)
+                    seq_count += 1
+                else:
+                    if align_length != len(record.seq):
+                        raise ValueError
+                    seq_count += 1
+        return (seq_count, align_length)
     
-    def fetch_column_V2(self, fasta_file, position, nb_column, vector="SDVector"):
+    def fetch_column(self, fasta_file, position, nb_column, vector="SDVector"):
         """
         Read the FASTA file and store 'nb_column' columns as SuccinctColumn objects in a list.
         To do that, it reads 'nb_column' nucleotides in each sequence, starting at the position 'position' in the file 'fasta_file'.
@@ -125,6 +130,51 @@ class SuccinctMultipleAlignment:
         nt_kept, previous_nt = [''] * nb_column, [''] * nb_column
         bit_vectors = []
         with open(fasta_file, "r") as handle:
+            for record in SeqIO.parse(handle, 'fasta'):
+                i = 0
+                while i < nb_column and position + i < self.__length:
+                    if seq_count == 0:
+                        bit_vectors.append(pysdsl.BitVector(self.__size))
+                        bit_vectors[i][seq_count] = 1
+                        nt_kept[i] += record.seq[position + i].upper()
+                        previous_nt[i] = record.seq[position + i]
+                    elif seq_count != 0 and previous_nt[i] != record.seq[position + i]:
+                        bit_vectors[i][seq_count] = 1
+                        nt_kept[i] += record.seq[position + i].upper()
+                        previous_nt[i] = record.seq[position + i]
+                    i += 1
+                seq_count += 1
+        sd_vector = []
+
+        for i in range(len(bit_vectors)):
+            sd_vector.append(SuccinctColumn(bit_vectors[i], nt_kept[i], vector=vector))
+        del bit_vectors, nt_kept, previous_nt
+        return sd_vector
+
+    def fetch_column_compress(self, fasta_file, position, nb_column, vector="SDVector"):
+        """
+        Read the FASTA file and store 'nb_column' columns as SuccinctColumn objects in a list.
+        To do that, it reads 'nb_column' nucleotides in each sequence, starting at the position 'position' in the file 'fasta_file'.
+
+        Parameters:
+        -----------
+        fasta_file : str
+            A FASTA file containing multiple sequences aligned.
+        position : int
+            The position in the sequence where the search starts.
+        nb_column : int
+            The number of columns to build in a single run.
+
+        Return:
+        -------
+        list(SuccinctColumn) :
+            A list of SuccinctColumns objects of size 'nb_column' corresponsing to columns of the multialignment starting at the
+            position 'position'.
+        """
+        seq_count = 0
+        nt_kept, previous_nt = [''] * nb_column, [''] * nb_column
+        bit_vectors = []
+        with gzip.open(fasta_file, 'rt') as handle:
             for record in SeqIO.parse(handle, 'fasta'):
                 i = 0
                 while i < nb_column and position + i < self.__length:
