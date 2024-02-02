5d6e4f759d9277f7b12d5bdd6d3d2b54107b3e32
awa13 <113526029+awa13@users.noreply.github.com>
2024-02-02 14:22:09 +0100

    Update script_test.py

--- script_test.py
+++ script_test.py
@@ -47,3 +47,57 @@ print(align.get_info())
 
 # print('nt_frequency() :')
 # print(column.nt_frequency())
+def time_profile(func, *args, **kwargs):
+    #Enregistre le temps au debut avant l'execution de la fonction
+    start_time = time.time()
+    ### appel de la fonction
+    result = func(*args, **kwargs)
+    ### temps apres l'execution
+    end_time = time.time()
+    # calcul de la durée d'execution
+    execution_time = end_time - start_time
+    
+    return result, execution_time
+
+def memory_profile_psutil(func, *args, **kwargs):
+    """ 
+    cette fonction mesure le temps, le temps d'exécution d'une fonction specifiée en utilisant 
+    le module 
+    """
+    #processus
+    process = psutil.Process(os.getpid())
+    # print(process)
+    # mesure l'utilisation de la memoire avant en convertissant les octets en méga-octets
+    memory_avant = process.memory_info().rss / (1024.0 ** 2)  # En méga-octets car 1 mega-octet == 1024 kiloctet
+    result = func(*args, **kwargs)
+    ##1 mega-octet == 1024 kiloctet
+    memory_apres = process.memory_info().rss / (1024.0 ** 2)
+    ## mesure de la memoire apres 
+    memory_usage_result = memory_avant - memory_apres
+    return result, memory_usage_result
+
+def plot_time_memory_psutil(func, *args, **kwargs):
+    # Temps
+    _, execution_time = time_profile(func, *args, **kwargs)
+    
+    # Mémoire
+    memory_usage_result = memory_profile_psutil(func, *args, **kwargs)
+    
+    # Génération des courbes
+    plt.figure(figsize=(10, 5))
+    #courbe du temps d'execution 
+    plt.subplot(1, 2, 1)
+    plt.bar(['Execution Time'], [execution_time], color='blue')
+    plt.ylabel('Time (s)')
+    plt.title('Execution Time')
+    # courbe de la memoire
+    plt.subplot(1, 2, 2)
+    plt.bar(['Memory Usage'], [memory_usage_result[1]], color='green')
+    plt.ylabel('Memory Usage (MiB)')
+    plt.title('Memory Usage')
+
+    plt.tight_layout()
+    plt.show()
+
+# Exemple d'utilisation :
+plot_time_memory_psutil(align.fetch_column_V2, 'SARS-CoV-2_MSA_file2.fasta', 0, 1000)
